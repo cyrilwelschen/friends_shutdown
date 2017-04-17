0e1d37b7d7e61918e059096a9e909d4eb6649dd3
Cyril Welschen <cj.welschen@gmail.com>
2017-04-17 17:45:50 +0200

    first outline of main

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def globals_in_place():
+    pass
+
+
+def start_setup_globals():
+    pass
+
+
+def main():
+    # Check if globals are in place
+    if not globals_in_place():
+        start_setup_globals()
+    # Load specific data
+
+    # Update data
+
+    # Send shutdown
+
+    # Start friends
+
+
+if __name__ == "__main__":
+    main()
